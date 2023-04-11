fd6eda0d4e332431f46c73468807846e31601d4e
stepanetsaleksandr <stepanetsaleksandr@gmail.com>
2023-04-11 18:15:53 +0300

    1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import TopBar from "../components/TopBar";
 import Footer from "../components/Footer";
 import styles from "../styles/Layout.module.css";
-import AppBar from "../components/AppBar";
 
 const Layout = ({ children }) => {
   return (
